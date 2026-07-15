f394dc4fb24a5648c312f69e2540ec193988229d
Egi-de <egidefiston@gmail.com>
2026-07-15 09:55:02 +0200

    Fix tasks 3, 6, 8, 101

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Elmwood"
 str2 = "Institute"
-str1 = "SET"
+str1 = str2[4] + str2[-4] + str2[4]
 print(f"Welcome to {str1} {str2}!")
--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[0:26] + word[31:39] + word[26:31] + word[39:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[44:83] + str[85:88] + str[0:6])
