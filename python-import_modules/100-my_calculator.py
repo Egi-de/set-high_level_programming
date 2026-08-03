d9dd3960cbb09036c1c6ffd893161ff69d880e82
Egi-de <egidefiston@gmail.com>
2026-08-03 23:24:29 +0200

    Add python import modules tasks

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+
+    operators = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if op not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, operators[op](a, b)))
---
+++ python-import_modules/README.md
@@ -0,0 +1,8 @@
+# Python - import & modules
+
+## Description
+
+This project covers importing modules and functions in Python.
+
+## Author
+Egi-de
