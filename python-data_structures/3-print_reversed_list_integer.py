ead993ab75dc76f65fff9f95014e03cfe5a46a2d
Egi-de <egidefiston@gmail.com>
2026-09-14 14:03:56 +0200

    0x03: use {:d} format spec to enforce integer type per checker

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for integer in (my_list or [])[::-1]:
-        print(str.format("{}", integer))
+        print("{:d}".format(integer))
