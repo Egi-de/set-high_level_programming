3a935374883cc6f6b2de45f1c21d8c9ffea2af5d
Egi-de <egidefiston@gmail.com>
2026-09-14 13:58:37 +0200

    0x03: handle None safely without changing function structure

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        my_list = []
-    for integer in my_list[::-1]:
+    for integer in (my_list or [])[::-1]:
         print("{}".format(integer))
