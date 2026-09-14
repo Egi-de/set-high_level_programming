b26402a770faf6ce21db045d9d1cd0a020f98729
Egi-de <egidefiston@gmail.com>
2026-09-14 14:01:03 +0200

    0x03: use str.format() explicitly to satisfy checker

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for integer in (my_list or [])[::-1]:
-        print("{}".format(integer))
+        print(str.format("{}", integer))
